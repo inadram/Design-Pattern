52751d4f147ff2ba14f67daeaa93e931d6d3198a
Amir Mardani <inadram@gmail.com>
2013-01-30 00:32:38 +0000

    Visitor Design Pattern
    more info at http://blog.inadram.com

---
+++ VisitorPattern/VisitorPattern/Payment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace VisitorPattern
+{
+    public class Payment
+    {
+        public int CalculateTheBill()
+        {
+            Printer printer=new Printer();
+            printer.PrinterList.Add(new Hp {Name = "HP100",Price = 100,Year = 1999});
+            printer.PrinterList.Add(new Hp { Name = "HP200", Price = 200, Year = 2010 });
+            printer.PrinterList.Add(new Sony { Name = "SonyPX1", Price = 220, Year = 2010, Discount = 70 });
+            printer.PrinterList.Add(new Toshiba { Name = "Toshiba600", Price = 800, Colorful = true });
+           
+            Billing billing=new Billing();
+            printer.Accept(billing);
+            return billing.totalPrice;
+        }
+       
+    }
+
+    [TestFixture]
+    public class TestPayment
+    {
+        [Test]
+        public void CalculateTheBill()
+        {
+            Payment payment=new Payment();
+            int calculateTheBill = payment.CalculateTheBill();
+            Assert.Pass(calculateTheBill.ToString());
+        }
+    }
+}
---
+++ VisitorPattern/VisitorPattern/Printer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisitorPattern
+{
+    public interface IVisitor
+    {
+        void Visitor(Hp hp);
+        void Visitor(Sony sony);
+        void Visitor(Toshiba toshiba);
+    }
+
+    public interface IMachine
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class Billing : IVisitor
+    {
+        public int totalPrice = 0;
+        public void Visitor(Hp hp)
+        {
+            totalPrice += hp.Price;
+        }
+
+        public void Visitor(Sony sony)
+        {
+            totalPrice += (sony.Price-sony.Discount);
+        }
+
+        public void Visitor(Toshiba toshiba)
+        {
+            totalPrice += (toshiba.Price);
+        }
+    }
+
+    public class Printer:IMachine
+    {
+        public List<IMachine> PrinterList = new List<IMachine>();
+
+
+        public void Accept(IVisitor visitor)
+        {
+            foreach (var machine in PrinterList)
+            {
+                machine.Accept(visitor);
+            }
+        }
+    }
+
+    public class Hp:IMachine
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Price     { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visitor(this);
+        }
+    }
+
+    public class Sony:IMachine
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public int Discount { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visitor(this);
+        }
+    }
+
+    public class Toshiba:IMachine
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool Colorful { get; set; }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visitor(this);
+        }
+    }
+}
